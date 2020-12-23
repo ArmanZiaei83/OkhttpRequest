65d9b6c8520d7dc38132f192b784eee67f90b03a
armanziaei <armanziaei83@gmaio.com>
2020-12-23 14:26:23 +0330

    Feat Calling Request : out request called using okhttp.

--- app/src/main/java/com/example/okhttprequest/MainActivity.java
+++ app/src/main/java/com/example/okhttprequest/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView_Result ;
@@ -22,6 +27,26 @@ public class MainActivity extends AppCompatActivity {
         Request request = new Request.Builder()
                 .url("https://jsonplaceholder.typicode.com/posts")
                 .build();
-        
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    final String myRes = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView_Result.setText(myRes);
+                        }
+                    });
+                }
+            }
+        });
     }
 }
